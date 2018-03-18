7e0cff2ff3f90914fd1e14601184b3fe70a1319f
kohsah <ashok@hariharan.org.in>
2018-03-18 18:34:12 +0530

    Simplified url_list.py

--- Lib/url_list.py
+++ Lib/url_list.py
@@ -1,15 +1,30 @@
 import json
 
-data = json.load(open('country_codes.json'))
+with open('countryCodes.json', encoding='utf-8') as jsonCountryCodes:
+    data = json.load(jsonCountryCodes)
+
 
 african_countries = list(
         filter(
-        lambda country: country['region-code'] == '002',
-        data['countries']['country']
+          lambda country: country['region-code'] == '002',
+          data['countries']['country']
+        )
     )
-)
 
-ccList = []
+#
+## ASHOK: Used String substituion for country code here to make it cleaerer
+# see http://thomas-cokelaer.info/blog/2011/05/python-string-substitution-using-dictionaries/
+#
+iloSourceCountryLink = "http://www.ilo.org/dyn/natlex/natlex4.countrySubjects?p_lang=en&p_country=%(country)s"
+
+ccList = [item['alpha-3'] for item in african_countries]
+
+listOfLinksToPages = [ iloSourceCountryLink % {'country': countryCode}  for countryCode in ccList]
+
+print("\n".join(listOfLinksToPages))
+
+# ASHOK much easier doing list comprehension and string substitution like above than a for loop
+""""
 i = 0
 for item in african_countries:
  ccList.insert(i,(item["alpha-3"]))
@@ -27,3 +42,4 @@ i = 0
 while i != len(urlList):
  print(urlList[i])
  i = i + 1
+"""
