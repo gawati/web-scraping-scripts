86702db9bc223d27e798372369bd2eaa79634115
Yash <yash.jajoo@gmail.com>
2018-03-28 13:17:37 +0530

    Updated html_download.py: - One command line argument. - Path to JSON files is now OS independent. - Moved get_documents_urls() to scrape_utils.py.
    Uploaded scrape_utils.py
    
    Uploaded "process_config" which contains all 58 JSON files.

--- Lib/html_download.py
+++ Lib/html_download.py
@@ -1,56 +1,47 @@
 import os
 import sys
-import json
 import time
-import functions_for_ilo_natlex as fin
+import scrape_utils as su
 
 
 def get_file_paths(country_codes):
     directory = os.path.join("process_config")
-    file_paths = [directory + "\\" + item.lower()+".json" for item in country_codes]
+    file_names = [item.lower()+".json" for item in country_codes]
+    file_paths = [os.path.join(directory,file_name) for file_name in file_names]
 
     return file_paths
 
-def get_document_urls(file_paths):
-    with open(file_paths, encoding="utf-8") as jsonFile:
-        data = json.load(jsonFile)
+def write_html_to_file(url, url_list, country_codes, country_code_no):
 
-    url_data = data["countries"]
-    url_list = [item['url'] for item in url_data]
-    return url_list
-
-def write_html_to_file(url_list,country_codes,j,country_code_no):
-    raw_html_data = fin.get_html_from_url(url_list[j])
+    raw_html_data = su.get_html_from_url(url)
     time.sleep(5)
-    isn = url_list[j].split('p_isn=')[1].split('&')[0]
+    isn = url_list[url_list.index(url)].split('p_isn=')[1].split('&')[0]
 
     file_name = isn + ".txt"
     file_path = os.path.join("unprocessed_outputs",country_codes[country_code_no].lower(),file_name)
+    print(file_path)
     directory = os.path.dirname(file_path)
     if not os.path.exists(directory):
         os.makedirs(directory)
     with open(file_path, "w+", encoding="utf-8") as textFile:
         textFile.write(raw_html_data)
 
-def main(file_no,country_code_no):
+def main(country_code_no):
 
-    country_codes = fin.get_country_codes()
+    country_codes = su.get_country_codes()
     file_paths = get_file_paths(country_codes)
 
     print("Fetching raw HTMl data for each document and saving it as a text file")
-    url_list = get_document_urls(file_paths[file_no])
+    url_list = su.get_document_urls(file_paths[country_code_no])
 
-    j = 0
-    while j < len(url_list):
-        write_html_to_file(url_list,country_codes,j,country_code_no)
-        j = j + 1
+    for url in url_list:
+        write_html_to_file(url, url_list, country_codes,country_code_no)
 
 '''
-Function takes two parameters: file no. and Country_code no.
-Both parameters must be equal. That is, if file_no. is 0 then country_code_no. must also be 0.
-Both parameters must be in quotes.
+Function takes one parameter: country_code_no. (0 to 57)
+The parameter must be in quotes.
 
-python html_download.py "0" "0" will download HTML data of all URLs in Algeria's JSON file (dza.json)
+python html_download.py "0" will fetch raw HTML data of all URLs within Algeria's (country code: 0 (DZA)) JSON file (dza.json)
 '''
 if __name__ == '__main__':
-    main(int(sys.argv[1]), int(sys.argv[2]))
+    main(int(sys.argv[1]))
--- Lib/scrape_utils.py
+++ Lib/scrape_utils.py
@@ -19,3 +19,12 @@ def get_html_from_url(url_list):
     r = requests.get(url_list)
     data = r.text
     return data
+	
+def get_document_urls(file_paths):
+    with open(file_paths, encoding="utf-8") as jsonFile:
+        data = json.load(jsonFile)
+
+    url_data = data["countries"]
+    url_list = [item['url'] for item in url_data]
+    return url_list
+	
